fdce47f4bf941825ed7e61dd45012f3b994257d7
muralinadh <muralinadh7@gmail.com>
2017-04-27 11:35:34 +0530

    Add files via upload

---
+++ LogisticsApp/src/main/java/com/murali/logistics/configuration/MvcConfiguration.java
@@ -0,0 +1,31 @@
+package com.murali.logistics.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.murali.logistics")
+public class MvcConfiguration extends WebMvcConfigurerAdapter {
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry){
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	
+	@Bean(name="viewResolver")
+	public InternalResourceViewResolver viewResolver(){
+		InternalResourceViewResolver irvr = new InternalResourceViewResolver();
+		
+		irvr.setPrefix("/WEB-INF/views/");
+		irvr.setSuffix(".jsp");
+		
+		return irvr;
+	}
+
+}
